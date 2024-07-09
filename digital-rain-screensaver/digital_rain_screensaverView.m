52e122aa3ac68828eac332fd6edcc783de02828c
Nathan Andrews <nandrews.programming@gmail.com>
2024-07-08 20:43:12 -0400

    Initial Commit

---
+++ digital-rain-screensaver/digital_rain_screensaverView.m
@@ -0,0 +1,51 @@
+//
+//  digital_rain_screensaverView.m
+//  digital-rain-screensaver
+//
+//  Created by Nathan Andrews on 7/8/24.
+//
+
+#import "digital_rain_screensaverView.h"
+
+@implementation digital_rain_screensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
