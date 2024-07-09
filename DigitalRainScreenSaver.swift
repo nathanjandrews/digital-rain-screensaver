7ba0e0a7fa85808f17572e307d6da5d35159a826
Nathan Andrews <nandrews.programming@gmail.com>
2024-07-08 21:01:29 -0400

    initial commit

---
+++ DigitalRainScreenSaver.swift
@@ -0,0 +1,62 @@
+//
+//  DigitalRainScreenSaver.swift
+//  digital-rain-screensaver
+//
+//  Created by Nathan Andrews on 7/8/24.
+//
+
+import Foundation
+import ScreenSaver
+
+class DigitalRainScreenSaver : ScreenSaverView {
+    private var screenHeight: Double = 0
+    private var screenWidth: Double = 0
+    
+    private let textView = NSTextView()
+    
+    override init?(frame: NSRect, isPreview: Bool) {
+        super.init(frame: frame, isPreview: isPreview)
+        
+        // Need to subscribe to "willStop" notification to terminate ScreenSaverView instances that
+        // have persisted between invocations of the screen saver. This is hack to get around a bug
+        // with the "legacyScreenSaver (wallpaper)" process that does not terminate when the screen
+        // saver terminates (it should though...). Hopefully this bug will be fixed and this
+        // subscription can be removed with no other code changes.
+        DistributedNotificationCenter.default.addObserver(
+                   self,
+                   selector: #selector(willStop(_:)),
+                   name: Notification.Name("com.apple.screensaver.willstop"),
+                   object: nil
+               )
+        
+        super.animationTimeInterval = 1 / 30
+        
+        self.screenWidth = super.frame.size.width
+        self.screenHeight = super.frame.size.height
+        
+        textView.string = "Hello, World!"
+        
+        super.addSubview(textView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /**
+     * Notification (event) handler that fires when the "willStop" notification is sent to this ScreenSaverView instance.
+     * This handler terminates the current instance, if the current instance is not the preview. Albeit a hack, this handler
+     * stops a memory leak with a MacOS screen saver process. Hopefully this bug will be fixed and this handler can
+     * be removed with no other code changes.
+     */
+    @objc func willStop(_ aNotification: Notification) {
+        if (!isPreview) {
+            NSApplication.shared.terminate(nil)
+        }
+    }
+    
+    override func animateOneFrame() {}
+ 
+    override var hasConfigureSheet: Bool { return false }
+    override var configureSheet: NSWindow? { return nil }
+}
